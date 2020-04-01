6881fa6694f82550d0697025e3831df67564f500
Lucas Rocha <lucas_r.rocha@outlook.com>
2020-04-01 15:02:38 -0300

    Draw the floor and sky of the game in canvas

---
+++ js/game/start.js
@@ -0,0 +1,19 @@
+'use strict';
+((win, doc) => {
+  const screen = {
+    width: win.innerWidth,
+    height: win.innerHeight
+  }
+  const $canvas = doc.querySelector('#game-dino');
+  const context = $canvas.getContext('2d');
+  const floor = new Floor(context, screen);
+  const sky = new Sky(context, screen);
+
+  $canvas.width = screen.width;
+  $canvas.height = screen.height;
+
+  context.globalCompositeOperation = "destination-over";
+
+  floor.draw();
+  sky.draw();
+})(window, document);
